3f78febdb91b9701281c8d488022a3b8b41ff67d
Lucas Fernandes <int.fernandes@gmail.com>
2024-04-30 15:47:42 -0300

    11

--- challenge.sql
+++ challenge.sql
@@ -1,7 +1,13 @@
 
 ======================== 11 =======================
 
-
+SELECT 
+f.Nome,
+g.Genero 
+FROM Filmes f
+INNER JOIN FilmesGenero fg ON f.Id = fg.IdFilme
+INNER JOIN Generos g ON fg.IdGenero = g.Id
+WHERE g.Genero = 'Mistério'
 
 ======================== 12 =======================
 
